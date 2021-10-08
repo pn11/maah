1cd2e5a92fd40aefbe0a6e6a385bfdfc810fb584
OKA Naoya <pn11@users.noreply.github.com>
2021-10-08 19:04:39 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>自衛隊東京大規模接種センター用ブックマークレット生成ツール</h1>
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
